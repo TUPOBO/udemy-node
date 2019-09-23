b4f63e0b3cf97ba3e8964b2b5ff7caeda6860f4f
tupobo <1028461931@qq.com>
2019-09-23 10:57:09 +0800

    Import Node.js Core Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,17 @@
-const fs = require('fs')
+// const add = require('./utils');
+// const sum = add(2, 8);
 
-fs.writeFileSync('notes.txt', `My name is LiuBo.\n`)
-const file = fs.openSync('notes.txt', 'a')
-fs.appendFileSync(file,`My age is 28!`)
+// console.log(sum);
+
+/**
+ * Challenge: Define and use a function in a new file
+ *
+ * 1. Create a new file called notes.js
+ * 2. Create getNotes function that returns 'Your notes ...'
+ * 3. Export getNotes function
+ * 4. From app.js, load in and call that function printing message to console
+ */
+
+const notesContent = require('./notes');
+
+console.log(notesContent());
