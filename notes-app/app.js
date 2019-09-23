5c00931e87d6f1c1a3963abeb561f4c96eaf504f
tupobo <1028461931@qq.com>
2019-09-23 14:43:32 +0800

    Printing in Color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,15 @@
-const validator = require('validator');
-
 const notesContent = require('./notes.js');
 
 console.log(notesContent());
 
-console.log(validator.isEmail('1028461931@qq.com'));
-console.log(validator.isURL('https:/github.com'));
+/**
+ * Challenge: Use the chalk library in your project
+ *
+ * 1. Install version 2.4.1 of chalk
+ * 2. Load chalk into app.js
+ * 3. Use it to print the string 'Success!' to the console in green
+ * 4. Test your work
+ */
+const chalk = require('chalk');
+
+console.log(chalk.bold.bgGreen('Success'));
