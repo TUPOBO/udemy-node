6b59236974b8c9f1cf2aedcf36a04f8f13db0337
tupobo <1028461931@qq.com>
2019-09-23 11:40:00 +0800

    gitignore

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,8 @@
-// const add = require('./utils');
-// const sum = add(2, 8);
+const validator = require('validator');
 
-// console.log(sum);
-
-/**
- * Challenge: Define and use a function in a new file
- *
- * 1. Create a new file called notes.js
- * 2. Create getNotes function that returns 'Your notes ...'
- * 3. Export getNotes function
- * 4. From app.js, load in and call that function printing message to console
- */
-
-const notesContent = require('./notes');
+const notesContent = require('./notes.js');
 
 console.log(notesContent());
+
+console.log(validator.isEmail('1028461931@qq.com'));
+console.log(validator.isURL('https:/github.com'));
