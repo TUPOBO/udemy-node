d4be6444358567f84f5ebaf849227c9af8561d33
tupobo <1028461931@qq.com>
2019-09-23 16:38:05 +0800

    Getting Input from Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,11 @@
 const notesContent = require('./notes.js');
-
-console.log(notesContent());
-
-/**
- * Challenge: Use the chalk library in your project
- *
- * 1. Install version 2.4.1 of chalk
- * 2. Load chalk into app.js
- * 3. Use it to print the string 'Success!' to the console in green
- * 4. Test your work
- */
 const chalk = require('chalk');
 
-console.log(chalk.bold.blue.underline('Success'));
+const command = process.argv[2];
+if (command === 'add') {
+	console.log('Adding note!');
+} else if (command === 'remove') {
+	console.log('Removing note!');
+} else if (command === 'delete') {
+	console.log('Deleting note!');
+}
