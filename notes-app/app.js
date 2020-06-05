8a0c6a6b77c3669310013456ddb0eef0e1d45a77
tupobo <1028461931@qq.com>
2020-06-05 11:27:02 +0800

    feat: Adding a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,58 +1,57 @@
-const chalk = require("chalk");
-const yargs = require("yargs");
-const notesContent = require("./notes.js");
-const getNotes = require("./notes");
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notesContent = require('./notes.js');
+const notes = require('./notes');
 
 // Customize yarg version
-yargs.version("1.1.0");
+yargs.version('1.1.0');
 
 // Create add command
 yargs.command({
-  command: "add",
-  describe: "Add a new note",
+  command: 'add',
+  describe: 'Add a new note',
   builder: {
     title: {
-      describe: "Note title",
+      describe: 'Note title',
       demandOption: true, // required
-      type: "string", // value type
+      type: 'string', // value type
     },
     body: {
-      describe: "Note body",
+      describe: 'Note body',
       demandOption: true, // required
-      type: "string", // value type
+      type: 'string', // value type
     },
   },
   handler: function (argv) {
-    console.log(`Title:${argv.title}`);
-    console.log(`Body:${argv.body}`);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
 // Create remove command
 yargs.command({
-  command: "remove",
-  describe: "Remove a new note",
+  command: 'remove',
+  describe: 'Remove a new note',
 
   handler: function (argv) {
-    console.log("Removing the note!", argv);
+    console.log('Removing the note!', argv);
   },
 });
 
 // Create list command
 yargs.command({
-  command: "list",
-  describe: "List your note",
+  command: 'list',
+  describe: 'List your note',
   handler: function () {
-    console.log("Listing out all notes!");
+    console.log('Listing out all notes!');
   },
 });
 
 // Create read command
 yargs.command({
-  command: "read",
-  describe: "Read a note",
+  command: 'read',
+  describe: 'Read a note',
   handler: function () {
-    console.log("Reading the note!");
+    console.log('Reading the note!');
   },
 });
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,42 @@
-function getNotes() {
-	return 'Your notes ...';
-}
+const fs = require('fs');
 
-module.exports = getNotes;
+const getNotes = function () {
+  return 'Your notes ...';
+};
+
+// 更新 note
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter((item) => item.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body,
+    });
+    saveNote(notes);
+    console.log('New note added!');
+  } else {
+    console.log('Note title taken!');
+  }
+};
+
+// 读取原有 notes 内容
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    const notes = JSON.parse(dataJSON);
+    return notes;
+  } catch (error) {
+    return [];
+  }
+};
+
+// 保存最新 notes 内容
+const saveNote = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+module.exports = { getNotes, addNote };
